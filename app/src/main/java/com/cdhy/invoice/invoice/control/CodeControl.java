f16b931298944ac9f0b985eab16055071978d3e9
Administrator <1104784402@qq.com>
2016-08-23 10:59:19 +0800

    Rejected commit: Default

--- app/src/main/java/com/cdhy/invoice/invoice/control/CodeControl.java
+++ app/src/main/java/com/cdhy/invoice/invoice/control/CodeControl.java
@@ -1,14 +1,20 @@
 package com.cdhy.invoice.invoice.control;
 
+import android.content.Context;
+
 import com.cdhy.invoice.invoice.BasePresenter;
 import com.cdhy.invoice.invoice.BaseView;
 
 public class CodeControl {
-    public interface View extends BaseView{
+    public interface View extends BaseView {
         String getPhone();
+
+        void getCodeSuccess();
+
+        void OnHttpListenerFailed(String error);
     }
 
-    public interface Presenter extends BasePresenter{
+    public interface Presenter extends BasePresenter {
         void getCode();
     }
 }
