20f620017d300040cddd7611d19b9ca1241f15aa
Administrator <1104784402@qq.com>
2016-08-23 10:59:32 +0800

    Rejected commit: Default

--- app/src/main/java/com/cdhy/invoice/invoice/control/CreateCompanyControl.java
+++ app/src/main/java/com/cdhy/invoice/invoice/control/CreateCompanyControl.java
@@ -3,30 +3,35 @@ package com.cdhy.invoice.invoice.control;
 import com.cdhy.invoice.invoice.BasePresenter;
 import com.cdhy.invoice.invoice.BaseView;
 import com.cdhy.invoice.invoice.model.Card.Data;
+import com.cdhy.invoice.invoice.model.VagueCompanyBean;
 
 
 public class CreateCompanyControl {
-    public interface View extends BaseView{
+    public interface View extends BaseView {
 
         void createCompanySuccess();
 
         void updateCompanySuccess();
 
-        void queryByCompanyNameSuccess();
+        void queryByCompanyNameSuccess(VagueCompanyBean mVagueCompanyBean);
 
-        void queryByCompanySHSuccess();
+        void queryByCompanySHSuccess(VagueCompanyBean mVagueCompanyBean);
 
         Data getCreateCompanyDescribe();
 
+        String getCompanyName();
+
+        String getCompanySH();
+
+        void OnHttpListenerFailed(String error);
+
     }
 
-    public interface Presenter extends BasePresenter{
-        void queryByCompanyName(String companyName);
+    public interface Presenter extends BasePresenter {
 
-        /**
-         * @param sh 税号
-         */
-        void queryByCompanySH(String sh);
+        void queryByCompanyName();
+
+        void queryByCompanySH();
 
         void createCompany();
 
